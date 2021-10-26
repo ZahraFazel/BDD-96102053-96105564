424e7fa4a94084ef6755d88c9cbbbd63eb6cf261
Zahra <zfazel1379@gmail.com>
2021-10-26 13:23:12 +0330

    Added reverse and square root to Calculator

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -6,4 +6,20 @@ public class Calculator
     {
         return a + b;
     }
+
+    public double rvs(int a)
+    {
+        if (a != 0)
+            return 1.0 / a;
+        else
+            return Double.NaN;
+    }
+
+    public double sqrt(int a)
+    {
+        if (a >= 0)
+            return Math.sqrt(a);
+        else
+            return -1;
+    }
 }
